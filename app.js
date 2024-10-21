56b01b31e3969b80577181d25190f8588c7ef579
ZachRober <nolethedgehogdie@gmail.com>
2024-10-21 13:20:05 -0400

    Setup HTML structure for the drawing application

---
+++ app.js
@@ -0,0 +1,8 @@
+const myCanvas = document.getElementById("drawing-canvas");
+const clearButton = document.getElementById("clear-canvas");
+const colorSelector = document.getElementById("color-selector");
+const myButtons = document.getElementsByName("radio-list");
+
+const ctx = myCanvas.getContext("2d");//giving the canvas 2d context
+
+
